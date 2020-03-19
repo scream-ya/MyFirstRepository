48ddbe89000e93b8c4f28b69a3147a94c25c157e
Kate <61821446+scream-ya@users.noreply.github.com>
2020-03-19 17:40:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ProductTable from './ProductTable';
+
+ReactDOM.render(<ProductTable />, document.getElementById('root'));
