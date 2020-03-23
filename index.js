f72c5a55f5b5c7252bd44a0362f30a4b87e3c458
Anatoly <iseekyouu@yandex.ru>
2020-03-23 21:47:11 +0700

    ss

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ProductTable from './ProductTable';
-
-ReactDOM.render(<ProductTable />, document.getElementById('root'));
