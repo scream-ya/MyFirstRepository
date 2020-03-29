5f37af5986e203ed59d0ad532fd6d0f0cacf95f8
Anatoly <iseekyouu@yandex.ru>
2020-03-29 18:25:14 +0700

    fix

--- src/constants/products.js
+++ src/constants/products.js
@@ -8,7 +8,8 @@ const products = [
     {category: "Electronics",    price: "$99.99",  stocked: true,  name: "iPod Touch"},
     {category: "Electronics",    price: "$399.99", stocked: false, name: "iPhone 5"},
     {category: "Electronics",    price: "$199.99", stocked: true,  name: "Nexus 7"},
-    {category: "Fruit",          price: "$12",     stocked: true,  name: "Banana"}
+    {category: "Fruit",          price: "$12",     stocked: true,  name: "Banana"},
+	{category: "Fruit",          price: "$9",      stocked: true,  name: "Pineapple"}
 ];
 
 export default products
